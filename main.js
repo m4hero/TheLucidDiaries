cb5ffcb9a617797ccd8fa9647d71b4f0ee5fc943
Mostafa <mostafa.sharaf.fep@gmail.com>
2017-07-23 15:14:47 -0700

    v1.11

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 var  Diaries = [
-    {chapter: "1",
-     track: "A",
+    {chapter: "Intro",
+     track: "X",
      page: 1,
      link: 'Lucid Diaries Entry Intro page 1.PDF'
     },
-     {chapter: "1",
-     track: "A",
+     {chapter: "Intro",
+     track: "X",
      page: 2,
      link: 'Lucid Diaries Entry Intro page 2.PDF'
     },
      {chapter: "1",
      track: "A",
+     page: 1,
+     link: 'Lucid Diaries Entry Chapter 1 A page 1.PDF'
+    },
+     {chapter: "1",
+     track: "A",
+     page: 2,
+     link: 'Lucid Diaries Entry Chapter 1 A page 2.PDF'
+    },
+     {chapter: "1",
+     track: "A",
      page: 3,
-     link: 'Lucid Diaries Entry Intro page 3.PDF'
-    },
-     {chapter: "1",
-     track: "A",
-     page: 4,
-     link: 'Lucid Diaries Entry Intro page 4.PDF'
-    },
-     {chapter: "1",
-     track: "A",
-     page: 5,
-     link: 'Lucid Diaries Entry Intro page 5.PDF'
+     link: 'Lucid Diaries Entry Chapter 1 A page 3.PDF'
     }
 ]
 var count = 0;
@@ -32,18 +32,19 @@ var count = 0;
 $(document).ready(function () {
 
   var picz = document.getElementById("#pic")
-    $("#hh").css("color","blue");
+    $("#hh").css("color","black");
 
     $("#next-arrow").click(function(){
         
-        $("#pic").animate({opacity: "toggle",height: "toggle"},2000,"swing")
+        $("#pic").animate({opacity: "toggle",height: "toggle"},2000);
+        
         $("#pic").attr("src", function(){
             count++
             return "" + Diaries[count].link
-        });
-        $("#pic").animate({opacity: "toggle",height: "toggle"},2000,"swing").delay(3000)
+        })
+        $("#pic").animate({opacity: "toggle",height: "toggle"},2000);
     });
-console.log(count);
+
 
 
     
